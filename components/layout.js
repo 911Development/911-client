721ba0a6448f4fb638f172aa1efa0642fcdd3070
hsyntes <se.hsyntes@gmail.com>
2024-06-16 03:37:58 +0300

    updated shadow colors

--- components/layout.js
+++ components/layout.js
@@ -43,22 +43,22 @@ const Layout = ({ Poppins_400, children }) => {
         <Shadow
           variant={"primary-lighter"}
           position={{ top: "5%", left: "10%" }}
-          opacity={0.035}
+          opacity={themeState.theme === "dark" ? 0.015 : 0.07}
         />
         <Shadow
           variant={"secondary-lighter"}
           position={{ top: "20%", right: "5%" }}
-          opacity={0.035}
+          opacity={themeState.theme === "dark" ? 0.015 : 0.07}
         />
         <Shadow
           variant={"secondary-lighter"}
           position={{ top: "50%", left: "10%" }}
-          opacity={0.035}
+          opacity={themeState.theme === "dark" ? 0.015 : 0.07}
         />
         <Shadow
           variant={"primary-lighter"}
           position={{ top: "90%", right: "0%" }}
-          opacity={0.035}
+          opacity={themeState.theme === "dark" ? 0.015 : 0.07}
         />
         {children}
       </main>
